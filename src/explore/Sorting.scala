bf228a9319e053e8260673f560bfbf1e71f6391e
Ralph Recto <recto.ralph@gmail.com>
2015-07-11 12:48:44 -0700

    first commit

---
+++ src/explore/Sorting.scala
@@ -0,0 +1,5 @@
+package explore
+
+class Sorting {
+
+}
---
+++ src/problems/Problem001.scala
@@ -0,0 +1,14 @@
+package problems
+
+/* 001: implement a regex engine with only characters and the ? operator i.e.
+ * 
+ */
+class Problem001 {
+
+  def match()
+
+  def main(args: Array[String]) = {
+
+  }
+
+}
