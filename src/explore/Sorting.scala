62f22af0123a7370af84e510aca7cee815d771a4
Ralph Recto <recto.ralph@gmail.com>
2015-07-11 13:27:04 -0700

    Implemented selection sort.

--- src/explore/Sorting.scala
+++ src/explore/Sorting.scala
@@ -1,5 +1,41 @@
-package explore
 
-class Sorting {
 
+object Sorting {
+  // For simplicity we will be doing ascending sorts
+
+  // O(n^2) sorts
+
+  // Idea: keep a "sorted" and "unsorted" section, repeatedly
+  // pull elements from unsorted and put them into sorted
+  def selectionSort(input: Array[Int]): Array[Int] = {
+    // Invariant: [0, start] is sorted
+    for (start <- 0 until input.length) {
+      val temp = input(start)
+      var min : Option[(Int, Int)] = None
+      // Find min in unsorted section
+      for (i <- start until input.length) {
+        min match {
+          case None => min = Some(input(i), i)
+          case Some((x, _)) => if (x > input(i)) min = Some(input(i), i)
+        }
+      }
+      // Switch min and value at the end of sorted section
+      min match {
+        case None => () // erronous case...
+        case Some((value, pos)) => {
+          input(start) = value
+          input(pos) = temp
+        }
+      }
+    }
+    input
+  }
+
+  def printArray[T](arg: Array[T]) = {
+    println(arg.toList)
+  }
+
+  def main(args: Array[String]) = {
+    printArray(selectionSort(Array(5, 7, 3, 4, 2, -100)))
+  }
 }
--- src/problems/Problem001.scala
+++ src/problems/Problem001.scala
@@ -1,14 +1,10 @@
 package problems
 
 /* 001: implement a regex engine with only characters and the ? operator i.e.
- * 
+ *
  */
 class Problem001 {
 
-  def match()
-
-  def main(args: Array[String]) = {
-
-  }
+  def main(args: Array[String]) = { }
 
 }
