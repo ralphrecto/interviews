5369cf26a94249dd02bd161302280522e911e311
Ralph Recto <recto.ralph@gmail.com>
2015-07-14 22:52:40 -0700

    added comments for BST balanced problem.

--- src/explore/Trees.scala
+++ src/explore/Trees.scala
@@ -16,8 +16,13 @@ object Trees {
   def maxHeight[T](tree: Tree[T]) = treeHeight(tree, Math.max)
 
   // precond: tree is a BST
-  // Idea: formalize "is balanced" to be equivalent to the statement
-  // abs(minHeight(tree)-maxHeight(tree)) <= 1
+  // Idea: formalize "is balanced" to be equivalent to the statement abs(minHeight(t) - maxHeight(t)) <= 1.
+  // Clearly if a tree t is balanced then its children are also. Suppose t.left and t.right are both balanced.
+  // If t.left and t.right have the same max height, then they also have the same min height, so t is balanced.
+  // If their max heights differ by more than 1 then they cannot possibly be balanced. Finally, consider the
+  // case when their max heights differ by exactly 1; suppose WLOG maxHeight(t.left) == n and maxHeight(t.right)
+  // == n+1. In the worst case, minHeight(t.left) == n-1 since t.left is balanced. But this implies that t cannot
+  // be balanced, hence we return false in this case.
   def isBalanced[T](tree: Tree[T]): Boolean = tree match {
     case Leaf(_) => true
     case Node(l, v, r) => {
